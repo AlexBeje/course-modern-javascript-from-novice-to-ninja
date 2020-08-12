e025588e8c7eea06bc8ed729553d991185a04ca9
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-12 22:32:06 +0200

    📦 NEW: Added the search filtering functionality

--- challanges/challange-two/app.js
+++ challanges/challange-two/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const todos = document.querySelector(".todos");
+const search = document.querySelector(".search input")
 
 const generateTemplate = todo => {
   const html = `
@@ -12,6 +13,7 @@ const generateTemplate = todo => {
   todos.innerHTML += html;
 }
 
+// add todos
 addForm.addEventListener("submit", e => {
   e.preventDefault();
   //* add -> the name of the addForm input
@@ -23,4 +25,30 @@ addForm.addEventListener("submit", e => {
     //* reset all the inputs of the form
     addForm.reset();
   };
+})
+
+// delete todos
+todos.addEventListener("click", e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+})
+
+// filter todos
+const filterTodos = (inputValue) => {
+  //* filter trough the todos children (using text content) and add a class to the ones that don't include the inserted input value
+  Array.from(todos.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  //* remove the previously added classes if the input value is modified / deleted
+  Array.from(todos.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => todo.classList.remove('filtered'));
+}
+
+// get the input value
+search.addEventListener("keyup", e => {
+  const inputValue = e.target.value.trim().toLowerCase();
+  filterTodos(inputValue);
 })
