15673ad8c12fa8ce4963bba37876b7be835f97c9
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-12 12:20:43 +0200

    📦 NEW: added the result section on the page

--- projects/project-one/app.js
+++ projects/project-one/app.js
@@ -1,12 +1,13 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
-  // the default action is to refresh the page.
+  //* the default action is to refresh the page.
   e.preventDefault();
 
   let score = 0;
-  // get the value of the form input named 'q1'
+  //* get the value of the form input named 'q1'
   const userAnswers = [
     form.q1.value,
     form.q2.value,
@@ -21,5 +22,8 @@ form.addEventListener("submit", (e) => {
     }
   });
 
-  console.log("😉", score);
+  // show result on page
+  //* search inside the .result element for a span element
+  result.querySelector("span").textContent = `${score}%`;
+  result.classList.remove("d-none");
 });
