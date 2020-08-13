ab539413c19198c24d151c0ec839960e3fcd31cd
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-13 12:35:27 +0200

    📦 NEW: Added the update ui on submit feature

--- projects/project-three/scripts/app.js
+++ projects/project-three/scripts/app.js
@@ -1,12 +1,33 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUi = data => {
+  const cityDetails = data.cityDetails;
+  const cityWeather = data.cityWeather;
+  
+  //* update the details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${cityWeather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${cityWeather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `
+
+  //* remove the display none
+  card.classList.remove('d-none');
+}
 
 const updateCity = async city => {
+  //* call of the async functions getCity() and getWeather()
   const cityDetails = await getCity(city);
   const cityWeather = await getWeather(cityDetails.Key);
 
   return {
-    cityDetails: cityDetails,
-    cityWeather: cityWeather
+    cityDetails,
+    cityWeather
   }
 }
 
@@ -20,7 +41,7 @@ cityForm.addEventListener('submit', e => {
 
   //update the ui with the new city value
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUi(data))
     .catch(err => console.log(err));
 
 })
