baa2c7d94234342d28e2037a74e43f1ac1e0e514
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-13 04:58:12 +0200

    📦 NEW: Created the fetch of the city

--- projects/project-three/scripts/app.js
+++ projects/project-three/scripts/app.js
@@ -1,1 +1,17 @@
-const key = 'foJW0EKqAslB0bemLUMWo47bcPzynnkw';
+const key = "foJW0EKqAslB0bemLUMWo47bcPzynnkw";
+
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  //* ? -> queries are added to the base string
+  //* & -> another querie
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('Lleida')
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
