058717f46c5010c93bfc1f34c2ed4c737b1a2943
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-17 13:35:46 +0200

    📦 NEW: Added the tooltip component

--- projects/project-five/src/index.js
+++ projects/project-five/src/index.js
@@ -1,1 +1,7 @@
-import './styles/style.css'
+import "./ninja-ui/styles/tooltip.css";
+import Tooltip from "./ninja-ui/tooltip";
+
+//create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
