08983ee6caa2a9b12c72a10511fe4c17974f28cb
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-17 17:08:57 +0200

    📦 NEW: Added the snackbar feature

--- projects/project-five/src/index.js
+++ projects/project-five/src/index.js
@@ -1,9 +1,11 @@
 import Tooltip from "./ninja-ui/tooltip";
-import "./ninja-ui/styles/tooltip.css";
 import Dropdown from "./ninja-ui/dropdown";
-import "./ninja-ui/styles/dropdown.css";
 import Tabs from "./ninja-ui/tabs";
+import Snackbar from "./ninja-ui/snackbar";
+import "./ninja-ui/styles/tooltip.css";
+import "./ninja-ui/styles/dropdown.css";
 import "./ninja-ui/styles/tabs.css";
+import "./ninja-ui/styles/snackbar.css";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
@@ -19,3 +21,12 @@ dropdowns.forEach((dropdown) => {
 // create tabs
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector(".snackbar-trigger");
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
+})
