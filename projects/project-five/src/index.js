0b99064e02a13fefe419941118c0e2263994d2d0
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-17 13:46:14 +0200

    📦 NEW: Added the dropdown component

--- projects/project-five/src/index.js
+++ projects/project-five/src/index.js
@@ -1,7 +1,15 @@
-import "./ninja-ui/styles/tooltip.css";
 import Tooltip from "./ninja-ui/tooltip";
+import "./ninja-ui/styles/tooltip.css";
+import Dropdown from "./ninja-ui/dropdown";
+import "./ninja-ui/styles/dropdown.css";
 
-//create tooltip
-const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
+// create tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+})
