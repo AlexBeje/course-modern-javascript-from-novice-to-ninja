0fd1410886d1c14539417859fa279eda86f4132a
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-17 03:45:44 +0200

    📦 NEW: Update the room functionality

--- projects/project-four/scripts/app.js
+++ projects/project-four/scripts/app.js
@@ -2,19 +2,21 @@
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
 const newNameForm = document.querySelector(".new-name");
-const updateMsg = document.querySelector(".update-mssg")
+const updateMsg = document.querySelector(".update-mssg");
+const rooms = document.querySelector(".chat-rooms");
 
 // add a new chat
-newChatForm.addEventListener("submit", e => {
+newChatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = newChatForm.message.value.trim();
-  chatroom.addChat(message)
+  chatroom
+    .addChat(message)
     .then(() => newChatForm.reset())
-    .catch(err => console.log(err));
-})
+    .catch((err) => console.log(err));
+});
 
 // update username
-newNameForm.addEventListener("submit", e => {
+newNameForm.addEventListener("submit", (e) => {
   e.preventDefault();
   // update name via chatroom
   const newName = newNameForm.name.value.trim();
@@ -24,12 +26,21 @@ newNameForm.addEventListener("submit", e => {
   // show then hide the update message
   updateMsg.innerHTML = `Your name was updated to ${newName}.`;
   setTimeout(() => {
-    updateMsg.innerHTML = '';
-  }, 3000)
-})
+    updateMsg.innerHTML = "";
+  }, 3000);
+});
 
 // check local storage for a name
-const username = localStorage.username ? localStorage.username : 'anon';
+const username = localStorage.username ? localStorage.username : "anon";
+
+// update the chat room
+rooms.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    chatUi.clear();
+  }
+  chatroom.updateRoom(e.target.getAttribute("id"));
+  chatroom.getChats((chat) => chatUi.render(chat));
+});
 
 // class instances
 const chatUi = new ChatUi(chatList);
